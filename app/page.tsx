ca8fb1125c40d5ac9eefdf8ef2d54cbb1565db4e
Luksonaa_Dev <lukazhozhadze53@gmail.com>
2025-06-15 10:14:13 +0400

    also fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import GridBackground from "./grid/page";
+import GridBackground from "./grid/GridBackground";
 import Header from "./components/header/page";
 
 export default function Home() {
